614e263deebb810a164a269248ffb10ce1cdbfb0
Vinicius Silva <fsvinicius9@gmail.com>
2026-08-03 11:32:25 -0300

    Add string manipulation, list operations, and dictionary access examples in numbers.py

--- django-lectures/numbers.py
+++ django-lectures/numbers.py
@@ -7,3 +7,31 @@ my_income = 100
 tax_rate = 0.1
 my_taxes = my_income * tax_rate
 print(my_taxes)
+mystring = "hello"
+print(mystring)
+
+mylist =  ['a','b','c','d']
+print(mylist[0])
+mylist.append('e')
+mylist.pop()
+print(mylist)
+
+matrix = [[1,2,3], [4,5,6], [7,8,9]]
+print(matrix[0][0])
+
+first_col = [row[0] for row in matrix]
+print(first_col)
+
+for num in mylist:
+    print(num)
+
+for num in mylist:
+    print(num)
+
+
+    # Dictionary
+
+    my_stuff = {'key1':'value1', 'key2':'value2','key3':{'123':[1,2,'grabMe']}}
+    print(my_stuff['key3']['123'][2])
+
+    
