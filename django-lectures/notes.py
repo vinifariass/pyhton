a571ef6e2cc77a9bfe568b15c1ce24cedd486319
Vinicius Silva <fsvinicius9@gmail.com>
2026-08-04 14:23:48 -0300

    Add examples of control flow, loops, and data structures in notes.py; expand on boolean, tuples, sets, and list comprehensions in numbers.py

---
+++ django-lectures/notes.py
@@ -0,0 +1,28 @@
+if 1<2:
+    print('first block')
+    if 20<3:
+        print('second block')
+
+if 1<2:
+    print('hello')
+else:
+    print('last')
+
+
+x = 0
+while x < 5:
+    print(f'the current value of x is {x}')
+    x += 1
+else:
+    print('x is not less than 5')
+
+
+seq = [1,2,3,4,5]
+
+for item in seq:
+    print(item)
+
+d={'sam':1,'frank':2,'dan':3}
+
+for k in d:
+    print(k)
--- django-lectures/numbers.py
+++ django-lectures/numbers.py
@@ -34,4 +34,60 @@ for num in mylist:
     my_stuff = {'key1':'value1', 'key2':'value2','key3':{'123':[1,2,'grabMe']}}
     print(my_stuff['key3']['123'][2])
 
-    
+    #Boolean
+
+    True or False
+
+    #Tuples - immutable sequence of objects
+
+    my_tuple = (1,2,3)
+    print(my_tuple[0])
+
+    #sets - unordered collection of unique elements
+
+    my_set = set()
+    my_set.add(1)
+    my_set.add(2)
+    my_set.add(3)
+    print(my_set)
+
+    #list comprehension
+
+    my_list = [1,2,3,4,5]
+
+s = 'django'
+#print 'd'
+print(s[0])
+#print 'o'
+print(s[-1])
+
+#print 'djan'
+print(s[:4])
+#print 'jan'
+print(s[4:])
+#print 'go'
+print(s[4:6])
+
+#list comprehensions
+
+my_list = [x for x in 'word']
+print(my_list)
+
+my_list = [x for x in range(0,11)]
+print(my_list)
+
+my_list = [x**2 for x in range(0,11)]
+print(my_list)
+
+
+#using keys and indexing, grabd the 'hello' string from the following dictionaries
+
+d = {'simple_key':'hello'}
+print(d['simple_key'])
+
+d2 = {'k1':{'k2':'hello'}}
+print(d2['k1']['k2'])
+d3 = {'k1':[{'nest_key':['this is deep',['hello']]}]}
+
+#Use print formatting to print the following string
+print("Hello my dog's name is {a} and he is {b} years old".format(a='Rex', b=4))
