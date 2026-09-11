6919b527e2abeb2a1edab3eba947b5a6742ea479
Vinicius Silva <fsvinicius9@gmail.com>
2026-09-11 15:50:50 -0300

    Refactor index view to display access records; update index.html for dynamic data rendering and improve template structure.

--- My_Django_Stuff/first_project/first_app/views.py
+++ My_Django_Stuff/first_project/first_app/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from first_app.models import Topic, Webpage, AccessRecord
 
 def index(request):
-    my_dict = {'insert_me': "Now I am coming from first_app/views.py!"}
-    return render(request, 'first_app/index.html', context=my_dict)
+    webpage_list = AccessRecord.objects.order_by('date')
+    date_dict = {'access_records': webpage_list}
+    return render(request, 'first_app/index.html', context=date_dict)
 # Create your views here.
--- My_Django_Stuff/first_project/first_project/settings.py
+++ My_Django_Stuff/first_project/first_project/settings.py
@@ -15,6 +15,7 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATE_DIR=os.path.join(BASE_DIR, 'templates')    
+STATIC_DIR=os.path.join(BASE_DIR, 'static')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
