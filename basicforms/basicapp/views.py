0bdefb6d1c9fa4680c4ad0a3bb7dc01a0ccebb2d
Vinicius Silva <fsvinicius9@gmail.com>
2026-09-12 22:43:57 -0300

    Enhance form handling and update templates for improved user experience; add email and message fields to form processing and refine HTML structure.

--- basicforms/basicapp/views.py
+++ basicforms/basicapp/views.py
@@ -7,4 +7,16 @@ def index(request):
 
 def form_page(request):
     form = forms.FormName()
+
+    if request.method == 'POST':
+        form = forms.FormName(request.POST)
+
+        if form.is_valid():
+            # Do something with the form data
+            print("Validation successful")
+            # CLeaned_data is a dictionary of validated form input fields
+            print("Name: " + form.cleaned_data['name'])
+            print("Email: " + form.cleaned_data['email'])
+            print("Message: " + form.cleaned_data['message'])
+            
     return render(request, 'form.html', {'form': form})
