df80007f447e23bf4519572b1cf34f0854123bab
Vinicius Silva <fsvinicius9@gmail.com>
2026-09-15 08:45:20 -0300

    Add HTML templates for base, index, other, and relative pages; update URL routing in urlpatterns

--- django_level_four/learning_templates/basic_app/urls.py
+++ django_level_four/learning_templates/basic_app/urls.py
@@ -1,9 +1,9 @@
-from django.conf.urls import url
+from django.urls import re_path
 from basic_app import views
 
-#TEMPLATE TAGGING
 app_name = 'basic_app'
-urlspatterns = [
-    url(r'^relative/$', views.relative, name='relative'),
-    url(r'^other/$', views.other, name='other'),
+
+urlpatterns = [
+    re_path(r'^relative/$', views.relative, name='relative'),
+    re_path(r'^other/$', views.other, name='other'),
 ]
